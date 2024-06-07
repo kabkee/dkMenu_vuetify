54f4e8ff4150d113d94dbbbcb3d99b72bd3dd031
Kabkee <kabkee.moon@gmail.com>
2024-06-07 22:01:58 +0900

    cors github.io origin 수정

--- amplify/backend/function/dkmenuvuetify82221692/src/index.js
+++ amplify/backend/function/dkmenuvuetify82221692/src/index.js
@@ -15,7 +15,7 @@ exports.handler = async (event) => {
     // console.log(`EVENT: ${JSON.stringify(event)}`);
 
     // 허용된 오리진 목록
-    const allowedOrigins = ['https://kabkee.github.io/dkMenu_vuetify', 'http://localhost:3001'];
+    const allowedOrigins = ['https://kabkee.github.io', 'http://localhost:3001'];
     const origin = event.headers.origin;
 
     // 요청한 오리진이 허용된 오리진 목록에 있는지 확인
